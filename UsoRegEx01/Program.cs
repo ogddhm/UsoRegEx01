10da2b1e4b405917b2641b8d4e98c24374f3522e
ogddhm <gduranapubli@gmail.com>
2020-02-16 00:59:31 +0100

    recorrer cadena desde bucle

--- UsoRegEx01/Program.cs
+++ UsoRegEx01/Program.cs
@@ -11,6 +11,24 @@ namespace UsoRegEx01
         static void Main(string[] args)
         {
             //aprender uso RegEx
+            string frase = "Mi nombre es Oscar y mi nº de tfno es (+34)123-45-67 y mi cOdigo postal es 28029";
+            int count = 0;
+            foreach(char i in frase)
+            {
+                if (i == 'O')
+                {
+                    
+                    count++;
+                }
+
+            }
+            if (count != 0)
+            {
+                Console.WriteLine("la cadena tiene O y el contador es {0}", count);
+            }
+                
+            else Console.WriteLine("no hay O mayuscula");
+
         }
     }
 }
